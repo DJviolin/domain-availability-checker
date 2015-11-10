3d6ab8560478f297d7095c72b037cef08086faeb
DJviolin <kerozin.joe@gmail.com>
2015-11-10 21:57:00 +0100

    wgwegrreg

--- check-domain-availability-jwhois-v3.sh
+++ check-domain-availability-jwhois-v3.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Based off of https://superuser.com/questions/433316/check-a-list-of-domains-with-the-whois-command
+
 while read domain; do
+
   printf '%s ' "$domain" # This way we know what whois is currently looking up before it completes.
-  jwhois "$domain" | grep -qci 'No Match'
-  if [ $? -ne 0 ]; then echo "REGISTERED"; else echo "AVAILABLE"; fi
+  jwhois "$domain" | egrep -qci 'No Match'
+  if
+    [ $? -ne 0 ]; then echo "REGISTERED"; else echo "AVAILABLE";
+  fi
+
 done
