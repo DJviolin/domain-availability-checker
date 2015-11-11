52f7116e9e8dcb8555aac66062bcc60b27c8e4f9
DJviolin <kerozin.joe@gmail.com>
2015-11-11 11:41:05 +0100

    refact

--- check-domain-availability-jwhois.sh
+++ check-domain-availability-jwhois.sh
@@ -13,6 +13,13 @@
 # WINDOWS: $ jwhois com. whois.iana.org
 # LINUX: $ jwhois -h whois.iana.org com
 # BASIC USAGE: jwhois domain.com
+# jwhois -c jwhois.conf --force-lookup --disable-cache domain.com
+
+# Configuration File (2015-09-12):
+# https://github.com/robert-scheck/jwhois/blob/master/example/jwhois.conf
+
+# BLACKLISTED:
+# [whois.ausregistry.net.au]
 
 if [ "$#" == "0" ]; then
     echo "You need tu supply at least one argument!"
@@ -22,12 +29,12 @@ fi
 ### GoDaddy TLDS: https://www.godaddy.com/tlds/gtld.aspx - Only checked: .global
 DOMAINS=( \
 '.com' \
-#'.hu' \
-#'.net' '.info' '.org' '.biz' '.mobi' \
-#'.eu' '.ru' '.co.uk' '.com.au' \
-#'.online' '.xyz' '.global' '.site' '.tech' '.space' '.news' '.club' '.rocks' '.design' '.company' '.life' '.website' \
-#'.tel' '.tv' '.cc' \
-#'.in' '.it' '.sk' \
+'.hu' \
+'.net' '.info' '.org' '.biz' '.mobi' \
+'.eu' '.ru' '.co.uk' '.com.au' \
+'.online' '.xyz' '.global' '.site' '.tech' '.space' '.news' '.club' '.rocks' '.design' '.company' '.life' '.website' \
+'.tel' '.tv' '.cc' \
+'.in' '.it' '.sk' \
 )
 
 ELEMENTS=${#DOMAINS[@]}
@@ -35,10 +42,11 @@ ELEMENTS=${#DOMAINS[@]}
 while (( "$#" )); do
 
   for (( i=0;i<$ELEMENTS;i++)); do
-      jwhois $1${DOMAINS[${i}]} | egrep -q \
-      '^No match|^NOT FOUND|^Not fo|AVAILABLE|^No Data Fou|has not been regi|No entri'
+      jwhois --force-lookup --disable-cache $1${DOMAINS[${i}]} | egrep -q \
+      'No match|^NOT FOUND|^Not fo|AVAILABLE|^No Data Fou|has not been regi|No entri'
     if [ $? -eq 0 ]; then
-        echo "$1${DOMAINS[${i}]} : available"; else
+        echo "$1${DOMAINS[${i}]} : available";
+    else
         echo "$1${DOMAINS[${i}]} : registered / not found";
     fi
   done
