782a2afe4efb57c274bdb3c17f197d46b8cdb900
DJviolin <kerozin.joe@gmail.com>
2015-11-14 11:18:50 +0100

    refact

--- node/index.js
+++ node/index.js
@@ -4,18 +4,7 @@
 
 // Usage:
 // https://nodejs.org/api/process.html#process_process_argv
-// $ node index.js one two=three four
-
-/*
-Output:
-0: C:\Program Files\nodejs\node.exe ░▒▓█▓▒░ 2015.11.5. 12:2:43:415
-1: c:\www\node\command-line-arguments\index.js ░▒▓█▓▒░ 2015.11.5. 12:2:43:415
-2: one ░▒▓█▓▒░ 2015.11.5. 12:2:43:415
-3: two=three ░▒▓█▓▒░ 2015.11.5. 12:2:43:415
-4: four ░▒▓█▓▒░ 2015.11.5. 12:2:43:415
-*/
-
-// SEPARATORS: ░▒▓█▓▒░
+// $ node index.js arg1 arg2
 
 // http://stackoverflow.com/questions/23023650/is-it-possible-to-pass-a-flag-to-gulp-to-have-it-run-tasks-in-different-ways
 // http://justindavis.co/2014/11/24/using-command-line-arguments-in-a-node-script/
@@ -38,8 +27,13 @@ var timeStamp = function() {
 var args = process.argv.slice(2);
 
 if (args.length <= 0) {
-  console.log("NO INPUT WAS DETECTED!\nFor get more help about the usage:\nUse the executible with \"-h\" or \"--help\" flags");
+  console.log("NO INPUT WAS DETECTED!\nFor get more help about the usage:\nUse the executible with \"-h\" or \"--help\" flags\nVersion info: \"-v\" or \"--version\"");
   process.exit(-1);
+}
+
+if(args == '-h' | args == '--help'){
+  console.log('This is the help page');
+} else {
 }
 
 var domainExtensions = ['.com', '.cm', '.biz']; // better to be const
@@ -51,9 +45,3 @@ args.forEach(function(val, index, array) {
     console.log(val + '' + tld + ' █ ' + timeStamp());
   });
 });
-
-/*if(args == '-f'){
-  console.log('Fahrenheit');
-} else {
-  console.log('Celsius');
-}*/
