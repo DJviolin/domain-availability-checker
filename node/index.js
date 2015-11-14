e5c4fb0535cbda327f1491481564f39c1abc6c86
DJviolin <kerozin.joe@gmail.com>
2015-11-14 14:24:37 +0100

    node

--- node/index.js
+++ node/index.js
@@ -10,6 +10,8 @@
 // http://justindavis.co/2014/11/24/using-command-line-arguments-in-a-node-script/
 // https://www.npmjs.com/package/yargs
 
+var version = 'v1.0.0';
+
 var timeStamp = function() {
   var d = new Date();
   var year = d.getFullYear();
@@ -19,21 +21,30 @@ var timeStamp = function() {
   var minutes = d.getMinutes();
   var seconds = d.getSeconds();
   var milliseconds = d.getMilliseconds();
-  //var time = year + '.' + month + '.' + day + '. ' + hours + ':' + minutes + ':' + seconds + ':' + milliseconds;
   var time = year + '.' + month + '.' + day + '. ' + hours + ':' + minutes + ':' + seconds;
   return time;
 };
 
 var args = process.argv.slice(2);
 
-if (args.length <= 0) {
+if (!~process.execArgv.indexOf('--harmony')) {
+  console.log('ERROR!\nES6 is required, Node.js must be run with \"--harmony\" flag!\nExample: $ node --harmony [APPNAME] [FLAGS] [ARGUMENTS]');
+  process.exit(-1);
+}
+
+if (args <= 0) {
   console.log("NO INPUT WAS DETECTED!\nFor get more help about the usage:\nUse the executible with \"-h\" or \"--help\" flags\nVersion info: \"-v\" or \"--version\"");
   process.exit(-1);
 }
 
-if(args == '-h' | args == '--help'){
+if((args == '-h' && args.length <= 1) || args == '--help'){
   console.log('This is the help page');
-} else {
+  process.exit(-1);
+}
+
+if(args == '-v' || args == '--version'){
+  console.log(version);
+  process.exit(-1);
 }
 
 var domainExtensions = ['.com', '.cm', '.biz']; // better to be const
