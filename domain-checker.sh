38f6a8dc30d38393db59b4c1736b1824dd17782a
DJviolin <kerozin.joe@gmail.com>
2015-11-25 10:29:41 +0100

    refact

--- domain-checker.sh
+++ domain-checker.sh
@@ -6,8 +6,7 @@
 # jwhois.conf
 # https://github.com/jonasob/jwhois/blob/master/example/jwhois.conf
 
-cat files/input-cache.txt > files/input.txt
-cat files/input.txt > files/input-cache.txt
+#cat files/input.txt > files/input-cache.txt
 
 DOMAINS='.com' # simple, space-separated list of domain suffixes
 
@@ -15,14 +14,14 @@ while read -r input; do
   for d in $DOMAINS; do
     MATCH=$(jwhois --force-lookup --disable-cache --no-redirect -c jwhois.conf "$input$d" | grep -oPa '^.*\b(Transferred Date|Updated Date|Creation Date|Registration Date|Expiration Date|REGISTRAR HOLD|REGISTRY HOLD|REDEMPTION GRACE PERIOD|REDEMPTIONPERIOD)\b.*$')
     if [ $? -eq 0 ]; then
-      echo "" |& tee --append files/registered.txt
-      echo -e "$input$d\tregistered\t"$(date +%y/%m/%d_%H:%M:%S)"\t$MATCH" | tr '\n' '\t' |& tee --append files/registered.txt
       #echo "" |& tee --append files/registered.txt
+      echo -e "$input$d\tregistered\t"$(date +%y/%m/%d_%H:%M:%S)"\t$MATCH" | tr '\n' '\t' |& tee --append files/registered.txt
+      echo "" |& tee --append files/registered.txt
     else
-      echo "" |& tee --append files/available.txt
-      echo -e "$input$d\tavailable\t"$(date +%y/%m/%d_%H:%M:%S)"\t$MATCH" | tr '\n' '\t' |& tee --append files/available.txt
       #echo "" |& tee --append files/available.txt
+      echo -e "$input$d\tavailable\t"$(date +%y/%m/%d_%H:%M:%S)"\t$MATCH" | tr '\n' '\t' |& tee --append files/available.txt
+      echo "" |& tee --append files/available.txt
     fi
   done
-  sed -i "/$input/d" files/input-cache.txt
+  #sed -i "/$input/d" files/input-cache.txt
 done < "$1"
