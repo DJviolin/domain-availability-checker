d37f46c86a36b5e6a9b07a9ab5e871b535c200ba
DJviolin <kerozin.joe@gmail.com>
2016-05-19 15:05:30 +0200

    refact

--- dcheck.sh
+++ dcheck.sh
@@ -14,14 +14,12 @@
 while read -r domain; do
   #MATCH=$(jwhois --force-lookup --disable-cache --no-redirect -c jwhois.conf "$domain$TLD" | grep -oPa '^.*\b(Updated Date|Creation Date|Expiration Date)\b.*\$')
   #MATCH=$(whois -H "$domain.com" | grep -oPa '^.*(Updated Date|Creation Date|Expiration Date).*$')
-  whois -H "$domain.com" | grep -oPaq '^.*(Updated Date|Creation Date|Expiration Date).*$'
+  whois -H $domain'.com' | grep -oPaq '^.*(Updated Date|Creation Date|Expiration Date).*$'
   if [ $? -eq 0 ]; then
-    #echo -e "$domain$TLD" |& tee --append files/registered.txt
-    echo "$domain.com" | tee --append files/registered.txt && echo "   registered"
+    echo $domain'.com' | tee --append files/registered.txt && echo '   registered'
     #echo "" |& tee --append remove/registered.txt
     #echo -e "$domain$TLD\tregistered\t$MATCH" | tr '\n' '\t' |& tee --append files/registered.txt
   else
-    #echo "$domain$TLD" |& tee --append files/available.txt
-    echo "$domain.com" | tee --append files/available.txt && echo "    available"
+    echo $domain'.com' | tee --append files/available.txt && echo '    available'
   fi
 done < files/domains.txt
