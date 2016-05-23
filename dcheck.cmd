5c30e2d7374681fa629cc840c6deaccbc8ae5ea0
DJviolin <kerozin.joe@gmail.com>
2016-05-23 17:17:51 +0200

    refact

--- dcheck.cmd
+++ dcheck.cmd
@@ -5,12 +5,13 @@ REM $ cd c:\www\bash\domain-availability-checker
 REM $ dcheck.cmd
 
 REM for /F "tokens=*" %%A in ("files/domains.txt") do [process] %%A
-set "TLD=.com"
 REM /F "tokens=*" %%i in (files/domains-win.txt) do %COMMAND% %%i ^| @echo %GREP%
 REM echo %DOMAINS%%TLD%
 REM whoiscl -r -n %DOMAINS%%TLD% | FINDSTR
 
+SET "TLD=.com"
+
 FOR /F "tokens=*" %%i in (%cd%\files\domains-win.txt) do (
-  REM do what you want with %%A here
-  whoiscl -r -n %%i ^| FINDSTR "Registrant Name:"
+  REM @echo %%i%TLD%
+  whoiscl -r -n %%i%TLD% ^| FINDSTR "lantosistvan.com"
 )
