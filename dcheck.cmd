9e3f512b3b710e1a101b7e4ca072e90a9a2b6764
DJviolin <kerozin.joe@gmail.com>
2016-05-24 11:03:24 +0200

    refact

--- dcheck.cmd
+++ dcheck.cmd
@@ -11,8 +11,19 @@ REM whoiscl -r -n %DOMAINS%%TLD% | FINDSTR
 
 SET "TLD=.com"
 
+ECHO Hello world>NUL
+
 FOR /F "tokens=*" %%i in (%cd%\files\domains-win.txt) do (
   REM @echo %%i%TLD%
-  REM whoiscl -r -n %%i%TLD% ^| FINDSTR "lantosistvan.com"
-  whoiscl -r -n %%i%TLD% | FINDSTR /R /I "\^.*Registrant Name.*$" 2>&1
+  REM whoiscl -r -n %%i%TLD% | FINDSTR /I /C:"Registrant Name" 2>&1
+  REM whoiscl -r -n %%i%TLD% | FINDSTR /I /C:"Registrant Name">NUL 2>&1
+  whoiscl -r -n %%i%TLD% | FINDSTR /I /C:"Registrant Name" 2>&1
+  SET "OUTPUT=%%i%TLD%"
+
+  IF not "%OUTPUT%" == "%%i%TLD%" (
+    ECHO Available: %%i%TLD%
+  ) ELSE (
+    ECHO Registered: %%i%TLD%
+  )
+
 )
