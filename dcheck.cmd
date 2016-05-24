788c0b151a7afd49202c15f9d1eea95e2108f83d
DJviolin <kerozin.joe@gmail.com>
2016-05-24 18:00:58 +0200

    refact

--- dcheck.cmd
+++ dcheck.cmd
@@ -1,4 +1,4 @@
-:: @ECHO OFF
+@ECHO OFF
 
 :: USAGE
 :: $ cd c:\www\bash\domain-availability-checker
@@ -7,16 +7,28 @@
 :: LOGGING
 :: $ dcheck.cmd > dcheck.log
 
-SET "TLD=.com"
+::SET "TLD=.com"
 
-FOR /F "tokens=*" %%i in (%cd%\files\domains-win.txt) do (
-  whoiscl -r -n %%i%TLD% | FINDSTR /I /C:"Registrant Name" > NUL 2>&1
-  SET "OUTPUT=%%i%TLD%"
+::TYPE %cd%\files\domains-win.txt
 
-  IF not "%OUTPUT%" == "" (
-    ECHO Registered: %%i%TLD%
-  ) ELSE (
-    ECHO Available: %%i%TLD%
-  )
+::FOR /F "tokens=*" %%i in (%cd%\files\domains-win.txt) do (
+::  whoiscl -r -n %%i%TLD% | FINDSTR /I /C:"Registrant Name" > NUL 2>&1
+::  SET "OUTPUT=%%i%TLD%"
 
-)
+::  IF not "%OUTPUT%" == "" (
+::    ECHO Registered: %%i%TLD%
+::  ) ELSE (
+::    ECHO Available: %%i%TLD%
+::  )
+
+::)
+
+
+SET count=1
+FOR /F "tokens=*" %%A IN (%cd%\files\domains-win.txt) DO (CALL :subroutine "%%A")
+GOTO :EOF
+
+:subroutine
+ ECHO %count%:%1
+ SET /A count+=1
+ GOTO :EOF
