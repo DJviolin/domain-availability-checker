5a5d9b4b8d4a1149f5e3b6b9eb12acb12145c49b
DJviolin <kerozin.joe@gmail.com>
2016-05-24 17:40:54 +0200

    refact

--- dcheck.cmd
+++ dcheck.cmd
@@ -10,14 +10,13 @@
 SET "TLD=.com"
 
 FOR /F "tokens=*" %%i in (%cd%\files\domains-win.txt) do (
-  SET (OUTPUT = whoiscl -r -n %%i%TLD% | FINDSTR /I /C:"Registrant Name" 2>&1)
-  :: SET "OUTPUT=%%i%TLD%"
-  ECHO %OUTPUT%
+  whoiscl -r -n %%i%TLD% | FINDSTR /I /C:"Registrant Name" > NUL 2>&1
+  SET "OUTPUT=%%i%TLD%"
 
-  :: IF not "%OUTPUT%" == "" (
-  ::   ECHO Registered: %%i%TLD%
-  :: ) ELSE (
-  ::   ECHO Available: %%i%TLD%
-  :: )
+  IF not "%OUTPUT%" == "" (
+    ECHO Registered: %%i%TLD%
+  ) ELSE (
+    ECHO Available: %%i%TLD%
+  )
 
 )
