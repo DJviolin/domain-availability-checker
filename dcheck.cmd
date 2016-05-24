8fd4e79a89dad4388897e33933a2769705d664a8
DJviolin <kerozin.joe@gmail.com>
2016-05-24 11:20:52 +0200

    refact

--- dcheck.cmd
+++ dcheck.cmd
@@ -15,7 +15,7 @@ FOR /F "tokens=*" %%i in (%cd%\files\domains-win.txt) do (
   REM @echo %%i%TLD%
   REM whoiscl -r -n %%i%TLD% | FINDSTR /I /C:"Registrant Name" 2>&1
   REM whoiscl -r -n %%i%TLD% | FINDSTR /I /C:"Registrant Name">NUL 2>&1
-  whoiscl -r -n %%i%TLD% | FINDSTR /I /C:"Registrant Name">NUL 2>&1
+  whoiscl -r -n %%i%TLD% | FINDSTR /I /C:"Registrant Name" > NUL 2>&1
   SET "OUTPUT=%%i%TLD%"
 
   IF not "%OUTPUT%" == "" (
